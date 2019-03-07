eef57c1d6e1cbfdd0d467ea83421985904f4e0db
dmeoded <dmeoded@gmail.com>
2019-03-07 14:33:32 +0200

    Add dummay login page and route

--- app/app.js
+++ app/app.js
@@ -2,13 +2,13 @@
 
 var app = angular.module("hoaSystem", ["ngRoute"]);
 
-app.config(function($routeProvider) {
+app.config(function ($routeProvider) {
     $routeProvider.when("/", {
         templateUrl: "app/home/home.html"
     }).when("/login", {
-        templateUrl: "app/login/login.html"
-        // controller: "loginCtrl"
- 
+        templateUrl: "app/login/login.html",
+        controller: "loginCtrl"
+
     }).otherwise({
         redirectTo: "/"
     })
