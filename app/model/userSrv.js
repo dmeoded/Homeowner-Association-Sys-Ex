8c536dbd8b50c8a7499651ff62ff1756f5d2fb79
dmeoded <dmeoded@gmail.com>
2019-03-07 16:17:34 +0200

    Add user service as not logged hard-coded

---
+++ app/model/userSrv.js
@@ -0,0 +1,58 @@
+app.factory("userSrv", function($http, $q, $log) {
+
+    var activeUser = null;
+
+    function User(genUser) {
+        this.id = genUser.id;
+        this.fullName = genUser.fullName;
+        this.email = genUser.email;
+        this.aptNo = genUser.aptNo;
+        this.flrNo = genUser.flrNo;
+        this.isCommitteeMember = genUser.isCommitteeMember;
+        this.communityId = genUser.communityId;
+    }
+
+    function login(email, pwd) {
+        var async = $q.defer();
+
+        $http.get("app/model/data/users.json").then(function(response) {
+            var users = response.data;
+            for (var i = 0; i < users.length; i++) {
+                if (users[i].email === email && users[i].pwd === pwd) {
+                    activeUser = new User(users[i]);
+                    async.resolve(activeUser);
+
+                }
+            }
+
+            if (!activeUser) {
+                async.reject("");
+            }
+        }, function(error) {
+            $log.error(error);
+            async.reject(error);
+        })
+
+        return async.promise;
+    }
+
+    function isLoggedIn() {
+        return activeUser ? true : false;
+    }
+
+    function logout() {
+        activeUser = null;
+    }
+
+    function getActiveUser() {
+        return activeUser;
+    }
+
+    return {
+        login: login,
+        isLoggedIn: isLoggedIn,
+        logout: logout,
+        getActiveUser: getActiveUser
+    }
+
+});
