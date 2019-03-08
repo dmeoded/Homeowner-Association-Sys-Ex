9c7023c928a0763fa789ab31553915fe60d8dc94
dmeoded <dmeoded@gmail.com>
2019-03-08 16:33:19 +0200

    Added signup dummy page that is available only to committee memeber user

--- app/app.js
+++ app/app.js
@@ -4,10 +4,13 @@ var app = angular.module("hoaSystem", ["ngRoute"]);
 app.config(function ($routeProvider) {
     $routeProvider.when("/", {
         templateUrl: "app/home/home.html"
+        
     }).when("/login", {
         templateUrl: "app/login/login.html",
         controller: "loginCtrl"
+
     }).when("/signup", {
+        templateUrl: "app/signup/signup.html"
 
     }).when("/dashboard", {
         templateUrl: "app/dashboard/dashboard.html",
@@ -18,14 +21,14 @@ app.config(function ($routeProvider) {
 
     }).when("/messages", {
         templateUrl: "app/messages/messages.html"
- 
+
     }).when("/issues", {
         templateUrl: "app/issues/issues.html"
- 
+
     }).when("/voting", {
         templateUrl: "app/voting/voting.html"
-  
-    }).when("/newTenant" , {
+
+    }).when("/newTenant", {
         templateUrl: "app//tenants/newTenant.html"
 
     }).otherwise({
