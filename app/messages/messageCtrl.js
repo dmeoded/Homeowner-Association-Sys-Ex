fd80d3c380c96d63418ce7554cdf2d2b70010c6a
dmeoded <dmeoded@gmail.com>
2019-03-16 11:01:04 +0200

    Fixes to the message page in the html, control and srv

--- app/messages/messageCtrl.js
+++ app/messages/messageCtrl.js
@@ -1,17 +1,61 @@
-app.controller("messageCtrl", function($scope, $location, userSrv) {
 
-    // $scope.invalidLogin = false;
-    // $scope.email = "dana@dana.com";
-    // $scope.pwd = "123";
-
-    // $scope.login = function() {
-
-    //     userSrv.login($scope.email, $scope.pwd).then(function(activeUser) {
-    //         $location.path("/dashboard");
-    //     }, function() {
-    //         $scope.invalidLogin = true;
-    //     });
-
-    // }
+    app.controller("messageCtrl", function($scope, $http, $location, $log, msgSrv, userSrv ) {
+    
+  
+    // Loading the messages
+    $scope.messages = [];
+    msgSrv.getMsgs().then(function(messages) {
+      $scope.messages = messages;
+    }, function(err){
+      $log.error(err);
+    })
+    
+  
+    // Filtering the messges by title and comment (presentation logic)
+    $scope.filterMsg = function(message) {
+      if (!$scope.filterBy) {
+        return true;
+      } else if (message.title.toLowerCase().includes($scope.filterBy.toLowerCase()) || 
+          message.comment.toLowerCase().includes($scope.filterBy.toLowerCase())) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    
+    // Order logic (presentation logic)
+    $scope.orderProp = "";
+    $scope.reverseOrder = false;
+    $scope.orderByProp = function(propName) {
+      
+      if ($scope.orderProp === propName) {
+        // Clicking header for the first time
+        $scope.reverseOrder = !$scope.reverseOrder;
+      } else {
+        // Clicking header for the first time
+        $scope.orderProp = propName;
+        $scope.reverseOrder = false;
+      }
+    }
+    
+    // Selected Message (presentation logic)
+    $scope.selectedMsg = null;
+    $scope.selectMsg = function(message) {
+      if ($scope.selectedMsg === message) {
+        $scope.selectedMsg = null;
+      } else {
+        $scope.selectedMsg = message;
+      }
+    //   $log.info("The selected message is: " + $scope.selectedMsg.title);
+    }
+    
+    $scope.addMsg = function() {
+    //   msgsrv.addMsg("Subaru", "B4");
+    }
+    
+    $scope.openMsg = function(message) {
+      $location.path("messages/" + $scope.messages.indexOf(message));
+    }
+  
 
 })
---
+++ app/model/msgSrv.js
@@ -0,0 +1,73 @@
+
+app.factory("msgSrv", function($log, $http, $q) {
+
+    var messages = [];
+    var wasEverLoaded = false;
+  
+    // Message constructor
+    function Message(MsgObject) {
+        this.id = MsgObject.id;
+        this.createdBy = MsgObject.createdBy;
+        this.createdAt = MsgObject.createdAt;
+        this.title = MsgObject.title;
+        this.prio = MsgObject.prio;
+        this.comment = MsgObject.comment
+    }
+    
+    function getMsgs() {
+      var async = $q.defer();
+      
+      if (wasEverLoaded) {
+        async.resolve(messages);
+      } else {
+        // Get all message from JSON - only for the first time
+        $http.get("app/model/data/messages.json").then(function(res) {
+          // success
+          for (var i = 0; i < res.data.length; i++) {
+            messages.push(new Message(res.data[i]));
+          }
+          wasEverLoaded = true;
+          async.resolve(messages); // resolving the promise with the messages array      
+        }, function(err) {
+          // error
+          async.reject(err);  // rejecting the promise
+        });
+      }
+      
+      return async.promise;
+    }    
+    
+    
+    function addMsg(MsgObject) {
+      var async = $q.defer();
+      
+      var newMsg = new Message(MsgObject);
+      messages.push(newMsg);
+      async.resolve(newMsg);
+    
+      return async.promise;
+    }
+    
+    
+    function getMsgById(id) {
+      var async = $q.defer();
+      
+      // Making sure that the messages are loaded
+      getMsgs().then(function(messages) {
+        async.resolve(messages[id]);  
+      }, function(err) {
+        async.reject(err);
+      });
+      
+      return async.promise;
+    } 
+    
+    
+    return {
+        getMsgs: getMsgs,
+        addMsg: addMsg,
+        getMsgById: getMsgById  
+    
+    }
+    
+  });
