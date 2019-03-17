abe42e337fe95da8afccc62be3940409f91f2d15
dmeoded <dmeoded@gmail.com>
2019-03-17 22:35:58 +0200

    Fix getUserFullNme to getUserFullName in all 3 current files

--- app/dashboard/dashboardCtrl.js
+++ app/dashboard/dashboardCtrl.js
@@ -2,7 +2,7 @@ app.controller("dashboardCtrl", function ($scope, $location, userSrv) {
 
     $scope.fullName = function () {
 
-        userSrv.getUserFullNme($scope.email, $scope.pwd).then(function (activeUser) {
+        userSrv.getUserFullName($scope.email, $scope.pwd).then(function (activeUser) {
             console.log("Hi Dana");
         });
 
--- app/model/userSrv.js
+++ app/model/userSrv.js
@@ -1,4 +1,4 @@
-app.factory("userSrv", function($http, $q, $log) {
+app.factory("userSrv", function ($http, $q, $log) {
 
     var activeUser = null;
 
@@ -6,22 +6,25 @@ app.factory("userSrv", function($http, $q, $log) {
         this.id = genUser.id;
         this.fullName = genUser.fullName;
         this.email = genUser.email;
+        this.tel = genUser.tel;
         this.aptNo = genUser.aptNo;
         this.flrNo = genUser.flrNo;
         this.isCommitteeMember = genUser.isCommitteeMember;
+        this.isHomeOwner = genUser.isHomeOwner;
         this.communityId = genUser.communityId;
+        this.remarks = genUser.remarks;
     }
 
     function login(email, pwd) {
         var async = $q.defer();
 
-        $http.get("app/model/data/users.json").then(function(response) {
+        $http.get("app/model/data/users.json").then(function (response) {
             var users = response.data;
             for (var i = 0; i < users.length; i++) {
                 if (users[i].email === email && users[i].pwd === pwd) {
                     activeUser = new User(users[i]);
-                    console.log( JSON.stringify( users[i]));
-                    console.log ("The active user in lop:" + JSON.stringify(activeUser));
+                    console.log("The current array user in login lop:", users[i]);
+                    console.log("The active user in login lop:", activeUser);
 
                     async.resolve(activeUser);
 
@@ -29,11 +32,11 @@ app.factory("userSrv", function($http, $q, $log) {
             }
 
             if (!activeUser) {
-                console.log ("The active user in lop - invalid!");
+                console.log("The active user in login lop - invalid!");
 
                 async.reject("");
             }
-        }, function(error) {
+        }, function (error) {
             $log.error(error);
             async.reject(error);
         })
@@ -45,7 +48,7 @@ app.factory("userSrv", function($http, $q, $log) {
     }
 
     function isCommitteeUser() {
-        console.log ("The active user is commettee member?" + JSON.stringify(activeUser));
+        console.log("The active user is commettee member?" + JSON.stringify(activeUser));
         return isLoggedIn() && activeUser.isCommitteeMember ? true : false;
     }
 
@@ -56,8 +59,8 @@ app.factory("userSrv", function($http, $q, $log) {
     function getActiveUser() {
         return activeUser;
     }
-    
-    function getUserFullNme() {
+
+    function getUserFullName() {
         return activeUser.fullName;
     }
 
@@ -67,7 +70,7 @@ app.factory("userSrv", function($http, $q, $log) {
         logout: logout,
         getActiveUser: getActiveUser,
         isCommitteeUser: isCommitteeUser,
-        getUserFullNme: getUserFullNme
+        getUserFullName: getUserFullName
     }
 
 });
