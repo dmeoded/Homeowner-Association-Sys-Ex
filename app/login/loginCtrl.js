37837b48127eea11d767b9ea2668c50356959779
dmeoded <dmeoded@gmail.com>
2019-03-20 16:23:31 +0200

    fix home page navbar

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,14 +1,14 @@
-app.controller("loginCtrl", function($scope, $location, userSrv) {
+app.controller("loginCtrl", function ($scope, $http, $location, $log, msgSrv, userSrv, genSrv) {
 
     $scope.invalidLogin = false;
     $scope.email = "dana@dana.com";
     $scope.pwd = "123";
 
-    $scope.login = function() {
+    $scope.login = function () {
 
-        userSrv.login($scope.email, $scope.pwd).then(function(activeUser) {
+        userSrv.login($scope.email, $scope.pwd).then(function (activeUser) {
             $location.path("/dashboard");
-        }, function() {
+        }, function () {
             $scope.invalidLogin = true;
         });
 
