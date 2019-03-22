008a838160763da0ed62ef593188c94443035495
dmeoded <dmeoded@gmail.com>
2019-03-22 15:50:43 +0200

    Tried to fix the date format in comment and break it to new line by </n> or <br>, but still not working

--- app/messages/updateMessageCtrl.js
+++ app/messages/updateMessageCtrl.js
@@ -1,8 +1,11 @@
 app.controller("updateMessageCtrl", function ($scope, $rootScope, msgSrv, userSrv, $routeParams, $location, $log) {
 
     $scope.updateMessage = function () {
-        $scope.message.comment =  
-        (!$scope.message.comment? " ": $scope.message.comment) + userSrv.getActiveUser().fullName +  $scope.ncomment;
+        $scope.message.comment =
+            (!$scope.message.comment ? " " : $scope.message.comment + "<BR>") + userSrv.getActiveUser().fullName 
+            + " created at " + (new Date().getTime("YYYY-MM-DD HH:MM:SS")) + ": " + $scope.ncomment;
+
+            // dateFormat(today, "YYYY-MM-DD HH:MM:SS");
         // msgSrv.updateMessage = function ($scope.message.id,  $scope.ncomment) {
         // }
         console.log("current message ID from Ctrl after Update: ", $scope.message.id);
