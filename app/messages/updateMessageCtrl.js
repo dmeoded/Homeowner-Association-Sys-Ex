58584de0cdb1939a34211fe69583ba5f58efda09
dmeoded <dmeoded@gmail.com>
2019-03-22 14:46:16 +0200

    Add active user to the comment. still with no design

--- app/messages/updateMessageCtrl.js
+++ app/messages/updateMessageCtrl.js
@@ -1,11 +1,13 @@
-app.controller("updateMessageCtrl", function ($scope, $rootScope, msgSrv, $routeParams, $location, $log) {
+app.controller("updateMessageCtrl", function ($scope, $rootScope, msgSrv, userSrv, $routeParams, $location, $log) {
 
     $scope.updateMessage = function () {
-        $scope.message.comment = $scope.message.comment + " " + $scope.ncomment;
+        $scope.message.comment =  
+        (!$scope.message.comment? " ": $scope.message.comment) + userSrv.getActiveUser().fullName +  $scope.ncomment;
         // msgSrv.updateMessage = function ($scope.message.id,  $scope.ncomment) {
         // }
         console.log("current message ID from Ctrl after Update: ", $scope.message.id);
-        console.log("current message Comm from Ctrl after Update: ", $scope.message.comment + " " + $scope.ncomment);
+        console.log("current message from Ctrl after Update: ", $scope.message);
+        console.log("current message Comm from Ctrl after Update: ", $scope.message.comment);
         $scope.showUpdMsg = false;
         $scope.ncomment = null;
     };
